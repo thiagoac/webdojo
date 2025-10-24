a67766d440b7e04f1901c415750ef64b8dcd04f6
Thiago Augusto da Costa <thiago.btw@gmail.com>
2025-10-23 21:28:53 -0300

    validando login e mensagem de usuário logado no painel

--- web/cypress/e2e/login.cy.js
+++ web/cypress/e2e/login.cy.js
@@ -1,6 +1,20 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Login', () => {
+  it('Deve logar com sucesso', () => {
     cy.viewport(1280, 720)
     cy.visit('http://localhost:3000/')
+
+    cy.get('#email').type('papito@webdojo.com')
+    cy.get('#password').type('katana123')
+
+    cy.contains('button','Entrar').click()
+
+    cy.get('[data-cy="user-name"]')
+      .should('be.visible')
+      .and('have.text', 'Fernando Papito')
+
+    cy.get('[data-cy="welcome-message"]')
+      .should('be.visible')
+      .and('have.text', 'Olá QA, esse é o seu Dojo para aprender Automação de Testes.')
+
   })
 })
